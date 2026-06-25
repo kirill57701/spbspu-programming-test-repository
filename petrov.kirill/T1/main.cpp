e813c1e449f85d29d280b0266d12d04e616a582f
Kirill <kirillpetrov56333@gmail.com>
2026-06-25 10:26:13 +0300

    plz work

--- petrov.kirill/T1/main.cpp
+++ petrov.kirill/T1/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+template<class T>
+concept mult = mult(T a, T b) {
+  {return a * b} -> convetile_to<T>;
+};
+
+template<mult T>
+T mult(T a, T b) {
+  return a * b;
+}
+
 int main() {
-  cout << "T0P 1T\n";
+  cout << "if this code dosent build, i will be funny\n";
 }
